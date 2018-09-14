0595c6676686f9889ceab39a2beb32cc75494d27
LDeWilfond18 <42647328+LDeWilfond18@users.noreply.github.com>
2018-09-14 09:50:20 -0500

    debug

--- Chapter3/src/YouDoIt/SpaService.java
+++ Chapter3/src/YouDoIt/SpaService.java
@@ -2,12 +2,27 @@ package YouDoIt;
 
 public class SpaService {
 	
-	private String serviceDescription;
-	private double price;
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	 private String serviceDescription;
+	   private double price;
+	   public SpaService()
+	   {
+	      serviceDescription = "XXX";
+	      price = 0;
+	   }
+	   public void setServiceDescription(String service)
+	   {
+	      serviceDescription = service;
+	   }
+	   public void setPrice(double pr)
+	   {
+	      price = pr;
+	   }
+	   public String getServiceDescription()
+	   {
+	      return serviceDescription;
+	   }
+	   public double getPrice()
+	   {
+	      return price;
+	   }
 	}
-
-}
